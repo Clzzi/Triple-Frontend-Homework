e3ac711e8ee8983afb112d695c58969584cf780c
Clzzi <clzzi1109@naver.com>
2022-07-04 17:52:46 +0900

    ADD: layout container & box

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,40 @@
 import '@/styles/reset.css'
+import styled from '@emotion/styled'
+
+import StatList from './components/StatList'
 import AwardList from './components/AwardList'
 import Layout from './components/common/Layout'
-import List from './components/common/List'
-import StatList from './components/StatList'
+import ApplicationLogo from './components/ApplicationLogo'
+
+const StatContainer = styled.div`
+  height: auto;
+  width: 1040px;
+  min-width: 1040px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+  margin: 0px 80px;
+`
+
+const StatWithAwardBox = styled.div`
+  width: 417px;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 84px;
+`
 
 const App = () => {
   return (
     <Layout>
-      <List direction="row">
-        <div>1</div>
-        <div>
+      <StatContainer>
+        <ApplicationLogo />
+        <StatWithAwardBox>
           <StatList />
           <AwardList />
-        </div>
-      </List>
+        </StatWithAwardBox>
+      </StatContainer>
     </Layout>
   )
 }
